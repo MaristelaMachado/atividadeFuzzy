ce93516932a5d14fdda82b95dc49bf1fd4c90f56
Maristela P. Machado <116725988+MaristelaMachado@users.noreply.github.com>
2022-12-05 14:43:44 -0300

    Add files via upload

---
+++ src/javafuzzyaula2022/JavaFuzzyAula2022.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafuzzyaula2022;
+
+/**
+ *
+ * @author 11872024602
+ */
+public class JavaFuzzyAula2022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmJFuzzyLogic form = new FrmJFuzzyLogic();
+        form.setVisible(true);
+    }
+    
+}
